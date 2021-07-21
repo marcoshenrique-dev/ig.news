f48a6b9fc2d7dda7681006e99e3bd63ab58c17c3
Marcos Henrique <marcoshenriqueh393@gmail.com>
2021-07-20 21:30:10 -0300

    feat: configurando projeto

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Início | ig.news</title>
+      </Head>
+
+    <h1>Hello <span>World</span></h1>
+    </>
+  )
+}
