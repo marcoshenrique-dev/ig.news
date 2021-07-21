5ed6c550fc5c0df66d010bf1b12dcb02f6eb5d39
Marcos Henrique <marcoshenriqueh393@gmail.com>
2021-07-21 09:29:19 -0300

    feat: criando componente header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+/* eslint-disable @next/next/no-img-element */
+
+import styles from "./styles.module.scss";
+
+export function Header() {
+    
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <a className={styles.active} href="">Home</a>
+          <a href="">Posts</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
