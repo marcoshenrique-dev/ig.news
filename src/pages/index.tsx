eeb1aaa47e6308e7cafc9897dc5bce302e549b46
Marcos Henrique <marcoshenriqueh393@gmail.com>
2021-07-21 21:37:03 -0300

    feat: estilizando página home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,25 @@
 import Head from "next/head";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
     <>
     <Head>
-      <title>Início | ig.news</title>
+      <title>Home | ig.news</title>
       </Head>
 
-    <h1>Hello <span>World</span></h1>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React </span>world.</h1>
+        <p>
+          Get access to all The publications <br />
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl Coding" />
+    </main>
     </>
   )
 }
