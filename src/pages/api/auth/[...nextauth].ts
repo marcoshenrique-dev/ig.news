8648bd8e5fb8571720f8274c228a3f7839329e68
Marcos Henrique <marcoshenriqueh393@gmail.com>
2021-07-23 16:19:17 -0300

    feat: configurando pagamentos no stripe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import {query as q} from "faunadb";
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import {fauna} from "../../../services/fauna";
 
 export default NextAuth({
   providers: [
@@ -8,5 +12,39 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: "read:user" 
     }),
+    
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const {email} = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index("user_by_email"),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection("users"), 
+              {data: {email}}
+            ),
+            q.Get(
+              q.Match(
+                q.Index("user_by_email"),
+                q.Casefold(user.email)
+              )
+            )
+            )
+          )
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+   }
 })
---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,73 @@
+import { query as q} from "faunadb";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/client";
+import { fauna } from "../../services/fauna";
+import { stripe } from "../../services/stripe";
+
+type User = {
+  ref: {
+    id: string;
+  }
+  data: {
+    stripe_costumer_id: string;
+  }
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if(
+    req.method === "POST"
+  ){
+
+    const session = await getSession({req});
+
+   
+    const user = await fauna.query<User>(q.Get(
+      q.Match(
+        q.Index("user_by_email"),
+        q.Casefold(session.user.email)
+      )
+    ));
+
+    let costumerId = user.data.stripe_costumer_id;
+
+    if(!costumerId){
+      const stripeCostumer = await stripe.customers.create({
+        email: session.user.email,
+        //metadata
+      });
+
+      await fauna.query(q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+        data: {
+          stripe_costumer_id: stripeCostumer.id,
+        }
+      }));
+
+      costumerId = stripeCostumer.id;
+  
+    }
+
+    
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: costumerId,
+      payment_method_types: ["card"],
+      billing_address_collection: "required",
+      line_items: [{
+        price: "price_1JFq5tEz7ASzIDdtnmNI3LIQ",
+        quantity: 1,
+      }],
+      mode: "subscription",
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+     
+    });
+
+    return res.status(200).json({sessionId: stripeCheckoutSession.id});
+
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).send("Method not allowed");
+  }
+}
